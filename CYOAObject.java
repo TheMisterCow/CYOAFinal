f5e800c93b6fc7a5736cc74088f0809033ee555e
TheMisterCow <105668283+TheMisterCow@users.noreply.github.com>
2022-05-16 12:02:27 -0600

    Update CYOAObject.java

--- CYOAObject.java
+++ CYOAObject.java
@@ -11,21 +11,21 @@ public class CYOAObject {
 	public String epilogue() {
 		return "Hello I am " + name +" the narrator of this story, today I will follow a along a story influencing the paths of its characters.\n "
 				+ "\nVery far away at the KFC next to Walmart an "
-				+ "odd boy named Todd sat eating a tub of the finest Kentucky fried chickenÖ\n"
+				+ "odd boy named Todd sat eating a tub of the finest Kentucky fried chicken‚Ä¶\n"
 				+ "\nTodd was a strange boy, all the people who met Todd said he was quite quirky. \n"
 				+ "Todd didn't care about what they said; Todd just kept on with life. \n"
 				+ "Todd kept a very strict schedule with his day. \n"
 				+ "First he would wake up and eat breakfast at exactly 7am, which was a fried chicken leg, \n"
 				+ "Todd then would head to work at 7:30. \n"
 				+ "Todd was an employee at Barn-A-Bargain which sold barns online. \n"
-				+ "Todd would work until his lunch break at exactly 12 oíclock. \n"
+				+ "Todd would work until his lunch break at exactly 12 o‚Äôclock. \n"
 				+ "Everyday he would go and get a bucket of Kentucky fried chicken from the KFC next to Walmart.\n"
 				+ "After his break ends, Todd would return to work at 12:45 and work until 7pm. \n"
-				+ "Todd would head home for dinner, eat another fried chicken leg, get ready for bed and go to sleep every day at 8pmÖ\n"
+				+ "Todd would head home for dinner, eat another fried chicken leg, get ready for bed and go to sleep every day at 8pm‚Ä¶\n"
 				+ "\nTodd just finished his bucket of chicken and was ready to head back to work. \n"
 				+ "Todd was on schedule and made it to the office at exactly 12:45. \n"
 				+ "Todd sat down at his desk to process orders as he did everyday. \n"
-				+ "'Today feels off,' Todd thought to himself, 'Itís a bit quiet today.' \n"
+				+ "'Today feels off,' Todd thought to himself, 'It‚Äôs a bit quiet today.' \n"
 				+ "Todd gets up and notices all the other employees are gone. \n"
 				+ "Todd has never seen the office with so little people. \n"
 				+ "Todd usually has at least 30 other employees working in the office with him. \n"
@@ -35,7 +35,7 @@ public class CYOAObject {
 				+ "Todd sits back down and continues doing work\r\n"
 				+ "\r\n"
 				+ "Option 2: \r\n"
-				+ "Todd goes to the bossí office to see if he missed a memo or meeting\r\n"
+				+ "Todd goes to the boss‚Äô office to see if he missed a memo or meeting\r\n"
 				+ "\r\n"
 				+ "Option 3:\r\n"
 				+ "Todd leaves the building and heads home\r\n"
@@ -58,8 +58,8 @@ public class CYOAObject {
 	}
 	
 	public String STwo() {
-		return "\nTodd decides the best thing to do is head to the bossí office to see if he missed a memo or a meeting.\n"
-				+ "Todd walks out of his office, down the hall, and through the big oak doors that leads to the bossí office.\n"
+		return "\nTodd decides the best thing to do is head to the boss‚Äô office to see if he missed a memo or a meeting.\n"
+				+ "Todd walks out of his office, down the hall, and through the big oak doors that leads to the boss‚Äô office.\n"
 				+ "When Todd enters the office it is uncanningly empty.\n"
 				+ "Everything in the room is untouched and organized as if nothing has happened in the room for weeks.\n"
 				+ "The only things in the room were a big red button on the wall and a computer on the desk.\n"
@@ -70,7 +70,7 @@ public class CYOAObject {
 				+ "Todd presses the big red button on the wall\r\n"
 				+ "\r\n"
 				+ "Option 2:\r\n"
-				+ "Todd gets onto the Bossí computer to try to find more information\r\n"
+				+ "Todd gets onto the Boss‚Äô computer to try to find more information\r\n"
 				+ "\nType 1 or 2 for your choice:";
 	}
 	
@@ -79,21 +79,21 @@ public class CYOAObject {
 				+ "While walking he notices that there is absolutely no one on the roads or anywhere.\n"
 				+ "Even more confused than before Todd starts walking home passing the KFC by the Walmart but not before noticing a strange building Todd has never noticed before.\n"
 				+ "The building had a big white bold sign that said Narrator.\n"
-				+ "It turned on Toddís curiosity but he didnít know if it was safe, it felt different, maybe Todd should just go home.\n"
+				+ "It turned on Todd‚Äôs curiosity but he didn‚Äôt know if it was safe, it felt different, maybe Todd should just go home.\n"
 				+ "\r\n"
 				+ "Option 1:\r\n"
 				+ "Todd goes and checks out the building the says Narrator\r\n"
 				+ "\r\n"
 				+ "Option 2:\r\n"
 				+ "Todd just ignores the building and heads home\r\n"
-				+ "\n Type 1 or 2 for your choice:";
+				+ "\nType 1 or 2 for your choice:";
 	}
 	
 	public String SFour() {
 		return "\nTodd is in shock, Todd seizes up and stands still, not knowing what to do.\n"
 				+ "Todd stares at the wall as minutes pass, those minutes turn to hours and yet nothing has changed.\n"
 				+ "Todd stands ther, waiting, waiting, and waiting, but for what?\n"
-				+ "Todd didnít know but he couldnít stop.\n"
+				+ "Todd didn‚Äôt know but he couldn‚Äôt stop.\n"
 				+ "Years pass and Todd stands there in the exact same spot he froze in shock.\n"
 				+ "Nothing has changed; it is as if time has frozen completely.\n"
 				+ "Todd would stand in that exact spot for eternity.\n"
@@ -105,15 +105,15 @@ public class CYOAObject {
 	public String SFive() {
 		return "Todd walks over to the big red button to enticing not to press.\n"
 				+ "As Todd slowly and dramatically moves his hand to the button it feels as if everything is slowing down.\n"
-				+ "ëWhat if this is wrong,í Todd thought, ëWhat if it kills me?í\n"
+				+ "‚ÄòWhat if this is wrong,‚Äô Todd thought, ‚ÄòWhat if it kills me?‚Äô\n"
 				+ "Todd hesitates and walks around the room a bit before pressing the big red button.\n"
 				+ "Sirens go off, and a big tv shows up in front of you.\n"
 				+ "The only thing the screen shows is the word FREEDOM!\n"
 				+ "Confused you stare at the tv when a big door opens up nearby leading to an outside world.\n"
 				+ "Not the one Todd is used to though.\n"
 				+ "When he steps out everything around him disappears, Todd is in a big white room and is congratulated by multiple scientists.\n"
-				+ "Todd asks them, ëHow? Why?î The scientists respond with, ëIt was all a simulation! You passed!í\n"
-				+ "Overwhelmed with emotion Todd didnít know what to say.\n"
+				+ "Todd asks them, ‚ÄòHow? Why?‚Äù The scientists respond with, ‚ÄòIt was all a simulation! You passed!‚Äô\n"
+				+ "Overwhelmed with emotion Todd didn‚Äôt know what to say.\n"
 				+ "Todd later went home that day, got some KFC, and went to bed, confused and both relieved to get out of the very confusing situations he had just witnessed.\n"
 				+ "The End\n"
 				+ "Ending 3/6\n"
@@ -121,8 +121,8 @@ public class CYOAObject {
 	}
 	
 	public String SSix() {
-		return "Todd walks over to the bossí computer and tries to log in.\n"
-				+ "Todd doesnít know the password and there was no possibility that it could be 3-4-5-1.\n"
+		return "Todd walks over to the boss‚Äô computer and tries to log in.\n"
+				+ "Todd doesn‚Äôt know the password and there was no possibility that it could be 3-4-5-1.\n"
 				+ "The only person to know that password would be " + name + " and the boss.\n"
 				+ "Surprisingly somehow by randomly typing in numbers into the computer Todd guessed that the pin was 3-4-5-1.\n"
 				+ "When he got onto the computer there was a single folder on the desktop.\n"
@@ -138,18 +138,18 @@ public class CYOAObject {
 	public String SSeven() {
 		return "Todd walks up to the building called Narrator.\n"
 				+ "Looks in throught the clear doors and notices a single person in an office and the end of a very long hallway\n"
-				+ "ëWait oh no,í says " + name + " ,ëthat might be badí.\n"
-				+ "Todd pushes on the door it doesnít budge.\n"
+				+ "‚ÄòWait oh no,‚Äô says " + name + " ,‚Äòthat might be bad‚Äô.\n"
+				+ "Todd pushes on the door it doesn‚Äôt budge.\n"
 				+ "Wanting to try to figure things out with the only person Todd has seen all day, so Todd tries to break the door.\n"
 				+ "Before Todd tries that though Todd notices that their is a sign on the door that says pull.\n"
 				+ "Todd then opens the door and starts walk down the long hallway and looks through the door.\n"
 				+ "On the desk of the person in the room it say " + name + ". "
 				+ name + " was talking into a mic that strangely resembled the thoughts that Todd was thinking.\n"
-				+ "Todd opens the door and enters the room.\n" + name + " looks around and sees Todd standing their behind him. ëHow did you get here?í he asks Todd.\n"
+				+ "Todd opens the door and enters the room.\n" + name + " looks around and sees Todd standing their behind him. ‚ÄòHow did you get here?‚Äô he asks Todd.\n"
 				+ "Todd just stands their and stares at the guy.\n"
-				+ "After a bit Todd says,í The front doors. Who are you?í\n" 
-				+ name + " responds with,í Why Iím " + name + "  and Iíve been the narrator of your life.\n"
-				+ "I influence everything you think and do and right know Iím going to influence you.í\n"
+				+ "After a bit Todd says,‚Äô The front doors. Who are you?‚Äô\n" 
+				+ name + " responds with,‚Äô Why I‚Äôm " + name + "  and I‚Äôve been the narrator of your life.\n"
+				+ "I influence everything you think and do and right know I‚Äôm going to influence you.‚Äô\n"
 				+ "Todd suddenly feels sleepy, " + name + " turns back to his desk and continues to speak into the mic just before Todd falls unconscious.\n"
 				+ "The next thing Todd knows he wakes up at his bustling work.\n"
 				+ "Everything seems to have turned back normal, Todd is mislead to think that what happened was all a dream and continues with his life as if nothing happened.\n"
@@ -162,7 +162,7 @@ public class CYOAObject {
 		return "Todd decides to ignore the very enticing building with the big sign that reads Narrator and go home.\n"
 				+ "Todd goes home and decides to go to bed and sleep until tomorrow.\n"
 				+ "Todd never woke up in the morning.\n"
-				+ "Toddís KFC diet had given him a heart attack at a very misfortunate time.\n"
+				+ "Todd‚Äôs KFC diet had given him a heart attack at a very misfortunate time.\n"
 				+ "With no one anywhere and nobody to save Todd, Todd would lay unmoving in bed.\n"
 				+ "Todd would never get to meet the narrator in the building named " + name + "."
 				+ "The End\n"
@@ -188,9 +188,17 @@ public class CYOAObject {
 			num = input.nextInt();
 			if(num == 1) {
 			System.out.println(SFive());
+			temp = input.next();
+			if(temp.equals("y")) {
+			play();
+			}
 			} else
 			if(num == 2) {
 			System.out.println(SSix());
+			temp = input.next();
+			if(temp.equals("y")) {
+			play();
+			}
 			} else
 			if(num != 1 && num != 2) {
 				System.out.println("You didn't input a usable number, retry?(y/n)");
@@ -204,10 +212,18 @@ public class CYOAObject {
 		System.out.println(SThree());
 			num = input.nextInt();
 			if(num == 1) {
-				System.out.println(SSeven());
+			System.out.println(SSeven());
+			temp = input.next();
+			if(temp.equals("y")) {
+			play();
+			}
 			} else
 			if(num == 2) {
 			System.out.println(SEight());
+			temp = input.next();
+			if(temp.equals("y")) {
+			play();
+			}
 			} else
 			if(num != 1 && num != 2) {
 				System.out.println("You didn't input a usable number, retry?(y/n)");
